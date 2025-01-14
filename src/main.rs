7afde069d2050754c37392b50cc8866a06a993f1
BiswajitThakur <biswajit733127@gmail.com>
2025-01-14 10:22:55 +0530

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,27 @@
-use std::{io::Write, net::TcpListener};
+use std::{
+    io::{self, Write},
+    net::TcpListener,
+};
 
-fn main() {
-    let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
+use codecrafters_http_server::{Method, Request};
+
+fn main() -> io::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:4221")?;
 
     for stream in listener.incoming() {
         match stream {
-            Ok(mut stream) => {
-                println!("accepted new connection");
-                stream.write_all(b"HTTP/1.1 200 OK\r\n\r\n").unwrap();
+            Ok(stream) => {
+                let mut req = Request::try_from(stream)?;
+                match (req.method, req.target.as_ref()) {
+                    (Method::Get, "/") => req.write_all(b"HTTP/1.1 200 OK\r\n\r\n")?,
+                    (Method::Get, _) => req.write_all(b"HTTP/1.1 404 Not Found\r\n\r\n")?,
+                    _ => {}
+                }
             }
             Err(e) => {
                 println!("error: {}", e);
             }
         }
     }
+    Ok(())
 }
---
+++ src/request.rs
@@ -0,0 +1,91 @@
+use std::{
+    borrow::Cow,
+    collections::HashMap,
+    io::{self, BufRead, BufReader, ErrorKind},
+    net::TcpStream,
+};
+
+#[derive(Debug, Clone, Copy)]
+pub enum Method {
+    Connect,
+    Delete,
+    Get,
+    Head,
+    Options,
+    Patch,
+    Post,
+    Put,
+    Trace,
+}
+
+pub struct Request<'a> {
+    pub method: Method,
+    pub target: Cow<'a, str>,
+    pub version: (u8, u8, u8),
+    headers: HashMap<Cow<'a, str>, Cow<'a, str>>,
+    stream: TcpStream,
+}
+
+impl<'a> Request<'a> {
+    pub fn get<T: AsRef<str>>(&self, value: &'a T) -> Option<&Cow<'a, str>> {
+        let value = value.as_ref();
+        self.headers.get(&Cow::Borrowed(value))
+    }
+}
+
+impl<'a> io::Read for Request<'a> {
+    fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> {
+        self.stream.read(buf)
+    }
+}
+
+impl<'a> io::Write for Request<'a> {
+    fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
+        self.stream.write(buf)
+    }
+    fn flush(&mut self) -> io::Result<()> {
+        self.stream.flush()
+    }
+}
+
+impl<'a> TryFrom<TcpStream> for Request<'a> {
+    type Error = io::Error;
+    fn try_from(value: TcpStream) -> Result<Self, Self::Error> {
+        let mut rdr = BufReader::new(&value).lines();
+        loop {
+            let line = rdr.next();
+            if line.is_none() {
+                continue;
+            }
+            let line = line.unwrap()?;
+            let vals: Vec<_> = line.split_whitespace().collect();
+            if vals.len() != 3 {
+                return Err(io::Error::new(ErrorKind::InvalidData, "Invalid Request"));
+            }
+            let method = match vals[0].to_lowercase().as_str() {
+                "connect" => Method::Connect,
+                "delete" => Method::Delete,
+                "get" => Method::Get,
+                "head" => Method::Head,
+                "options" => Method::Options,
+                "patch" => Method::Patch,
+                "post" => Method::Post,
+                "put" => Method::Put,
+                "trace" => Method::Trace,
+                _ => return Err(io::Error::new(ErrorKind::InvalidData, "Invalid Request")),
+            };
+            let target: Cow<'_, str> = Cow::Owned(vals[1].to_owned());
+            let version = match vals[2] {
+                "HTTP/1.1" => (1, 1, 0),
+                _ => return Err(io::Error::new(ErrorKind::InvalidData, "Invalid Request")),
+            };
+            return Ok(Self {
+                method,
+                target,
+                version,
+                headers: HashMap::default(),
+                stream: value,
+            });
+        }
+    }
+}
