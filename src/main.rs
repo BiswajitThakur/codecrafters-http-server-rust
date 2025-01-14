03dacc90b7a8095fe988662d58e0124d9e43635f
BiswajitThakur <biswajit733127@gmail.com>
2025-01-14 14:21:44 +0530

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::{
     io::{self, Cursor},
-    net::TcpListener,
+    net::{TcpListener, TcpStream},
+    thread,
 };
 
 use codecrafters_http_server::{Method, Request, Response, Status};
@@ -11,42 +12,47 @@ fn main() -> io::Result<()> {
     for stream in listener.incoming() {
         match stream {
             Ok(stream) => {
-                let req = Request::try_from(stream)?;
-                match (req.method, req.target.clone().as_ref()) {
-                    (Method::Get, v) => {
-                        let paths: Vec<&str> = v.split('/').filter(|v| !v.is_empty()).collect();
-                        if paths.is_empty() {
-                            return default_res().status(Status::OK).send_to(req);
-                        }
-                        match paths[0] {
-                            "echo" => default_res()
-                                .status(Status::OK)
-                                .content_type("text/plain")
-                                .content_length(paths[1].len())
-                                .body(Cursor::new(paths[1]))
-                                .send_to(req)?,
-                            "user-agent" => {
-                                let body = req.get("User-Agent").unwrap().clone();
-                                let body = body.as_ref();
-                                default_res()
-                                    .status(Status::OK)
-                                    .content_type("text/plain")
-                                    .content_length(body.len())
-                                    .body(Cursor::new(body))
-                                    .send_to(req)?;
-                            }
-                            _ => {
-                                default_res().send_to(req)?;
-                            }
-                        }
-                    }
-                    _ => {}
-                }
+                thread::spawn(move || handler(stream));
             }
             Err(e) => {
                 println!("error: {}", e);
             }
         }
+    }
+    Ok(())
+}
+
+fn handler(stream: TcpStream) -> io::Result<()> {
+    let req = Request::try_from(stream)?;
+    match (req.method, req.target.clone().as_ref()) {
+        (Method::Get, v) => {
+            let paths: Vec<&str> = v.split('/').filter(|v| !v.is_empty()).collect();
+            if paths.is_empty() {
+                return default_res().status(Status::OK).send_to(req);
+            }
+            match paths[0] {
+                "echo" => default_res()
+                    .status(Status::OK)
+                    .content_type("text/plain")
+                    .content_length(paths[1].len())
+                    .body(Cursor::new(paths[1]))
+                    .send_to(req)?,
+                "user-agent" => {
+                    let body = req.get("User-Agent").unwrap().clone();
+                    let body = body.as_ref();
+                    default_res()
+                        .status(Status::OK)
+                        .content_type("text/plain")
+                        .content_length(body.len())
+                        .body(Cursor::new(body))
+                        .send_to(req)?;
+                }
+                _ => {
+                    default_res().send_to(req)?;
+                }
+            }
+        }
+        _ => {}
     }
     Ok(())
 }
